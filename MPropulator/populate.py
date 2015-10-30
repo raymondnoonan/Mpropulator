f44703621d5f59d056518e13e55bd4dd5ef488a1
Ray Noonan <rnoonan@RNOONAN.mathematica.net>
2015-10-30 12:26:55 -0400

    Write most of populate and write_tab functionality

--- MPropulator/populate.py
+++ MPropulator/populate.py
@@ -1,57 +1,72 @@
-# TODO:
+import helpers
+import pandas as pd
+import openpyxl
+import ast
+import string
 
-def populate(...):
-    tabs = parse_config(...)
-    workbook = MPRWorkbook()
-    for tab in tabs:
-        tab.write_all(...)
-    workbook.save
-
-def parse_config(...):
-    tabs_to_return = []    
-    for row in config_file:
-        new_tab = Tab()
-        new_tab.start_cell = ...
-        ...
-        tabs_to_return.append(new_tab)
-    return tabs_to_return
+def populate(config, shell_xls, output_xls):
+    """Populates an Excel spreadsheet from a shell.
     
-class Table(object):
-
-    def __init__(self, <config_information>):
-        <initialize config attributes>
-        skip_rows
-        skip_columns
-        start_cell (aka first_cell)
-        csv_startcell        
-        csv
-        last_cell          
-          
-    def write(self):
-        # deal with it
-             
-class Tab(openpyxl.Sheet):
-        
-    def __init__(self, tables):
-        self.tables = tables
-        self.tab_name = tab_name        
-        
-    def overlap(self):
-        # deal with it        
-        
-    def write_all(self):
-        for table in self.tables:
-            table.write()
+    config: string of CSV file address containing config file
+    shell_xls: string of XLS file address containing Excel shell
+    output_xls: string of XLS file address for output file
     
-class MPRWorkbook(openpyxl.workbook):
-    def __init__(self):
-        pass
+    TODO: skip appropriate columns
+    """
+    workbook = openpyxl.load_workbook(shell_xls)    
+    
+    convert_cols = {
+    'csv_startcell' : ast.literal_eval, 
+    'skiprows' : ast.literal_eval,
+    'skipcols' : ast.literal_eval
+    }
+    
+    parsed_config = pd.read_csv(config, converters=convert_cols)
+    parsed_config.fillna(False, inplace=True)
+    
+    for table in parsed_config.iterrows():
+        if table['ignore'] is not True:        
+            sheet = workbook.get_sheet_by_name(table['tabname'])        
+            csv_startcell = table['csv_startcell']
+            
+            table_data = pd.read_csv(table['csv'], skiprows=csv_startcell[0]) 
+            # TODO: skip appropriate columns here            
+            
+            write_tab(sheet, table_data, table['tab_startcell'], \
+                table['skiprows'], table['skipcols'])
+        
+    workbook.save(output_xls)
 
-# Create config class?
-# No- it's just data storage
-
-# Parse config file
-# skip_rows
-
-# Read in CSV(s) of data
-# Write to Excel
+def write_tab(sheet, table_data, xls_startcell, skiprows, skipcols):
+    """Writes the data for a particular table to the corresponding
+    Excel spreadsheet.
+    
+    sheet: openpyxl worksheet to which you're writing
+    table_data: pandas data frame containing data to write
+    xls_startcell: cell in the sheet at which you will begin writing
+    skiprows: list of rows in Excel spreadsheet to skip
+    skipcols: list of columns in Excel spreadsheet to skip
+    """
+    num_rows = table_data.shape[0]
+    num_cols = table_data.shape[1]
+    
+    start_row = int(xls_startcell.translate(None, string.ascii_letters))
+    start_col = helpers.col_to_number(xls_startcell.translate(None, string.digits))
+    
+    num_skipcols = [helpers.col_to_number(col) for col in skipcols]    
+    
+    rows_to_write = [row for row in range(start_row, start_row + num_rows) if \
+        row not in skiprows]
+    cols_to_write = [col for col in range(start_col, start_col + num_cols) if \
+        col not in num_skipcols]
+    
+    for row_idx, row in enumerate(rows_to_write):
+        for col_idx, col in enumerate(cols_to_write):
+            current_cell = helpers.cell_name(row, col)
+            
+            # This will not work if the numpy data type cannot be converted
+            # easily into a native python one.
+            sheet[current_cell] = table_data.iloc[row_idx, col_idx].item()
+            
+    
+        
