c3733cecf6538f83ffd5bdfaba532e3c00666806
raymondnoonan <ray.noonan32@gmail.com>
2015-11-03 20:36:45 -0500

    Fix whitespace/styling to satsify Sublime's python linter

--- MPropulator/populate.py
+++ MPropulator/populate.py
@@ -4,50 +4,51 @@ import openpyxl
 import ast
 import string
 
+
 def populate(config, shell_xls, output_xls):
     """Populates an Excel spreadsheet from a shell.
-    
+
     config: string of CSV file address containing config file
     shell_xls: string of XLS file address containing Excel shell
     output_xls: string of XLS file address for output file
     """
-    workbook = openpyxl.load_workbook(shell_xls)    
-    
+    workbook = openpyxl.load_workbook(shell_xls)
+
     convert_cols = {
-    'csv_startcell' : ast.literal_eval, 
-    'skiprows' : ast.literal_eval,
-    'skipcols' : ast.literal_eval
+        'csv_startcell': ast.literal_eval,
+        'skiprows': ast.literal_eval,
+        'skipcols': ast.literal_eval
     }
-    
+
     parsed_config = pd.read_csv(config, converters=convert_cols)
     parsed_config.fillna(False, inplace=True)
-    
+
     for table in parsed_config.iterrows():
-        if table['ignore'] is not True:        
-            sheet = workbook.get_sheet_by_name(table['tabname'])    
-            
+        if table['ignore'] is not True:
+            sheet = workbook.get_sheet_by_name(table['tabname'])
+
             csv_startcell = table['csv_startcell']
             csv_start_row = csv_startcell[0]
-            csv_start_col = csv_startcell[1]            
-            
+            csv_start_col = csv_startcell[1]
+
             table_data = pd.read_csv(table['csv'], skiprows=csv_start_row)
-            
+
             num_cols = table_data.shape[1]
             cols_to_drop = [x for x in range(csv_start_col, num_cols)]
-            
-            table_data.drop(table_data.columns[cols_to_drop], axis=1, \
-                inplace=True)
-            
-            write_tab(sheet, table_data, table['tab_startcell'], \
-                table['skiprows'], table['skipcols'])
-        
+
+            table_data.drop(table_data.columns[cols_to_drop], axis=1,
+                            inplace=True)
+
+            write_tab(sheet, table_data, table['tab_startcell'],
+                      table['skiprows'], table['skipcols'])
+
     workbook.save(output_xls)
 
 
 def write_tab(sheet, table_data, xls_startcell, skiprows, skipcols):
     """Writes the data for a particular table to the corresponding
     Excel spreadsheet.
-    
+
     sheet: openpyxl worksheet to which you're writing
     table_data: pandas data frame containing data to write
     xls_startcell: cell in the sheet at which you will begin writing
@@ -56,22 +57,22 @@ def write_tab(sheet, table_data, xls_startcell, skiprows, skipcols):
     """
     num_rows = table_data.shape[0]
     num_cols = table_data.shape[1]
-    
+
     start_row = int(xls_startcell.translate(None, string.ascii_letters))
-    start_col = helpers.col_to_number(xls_startcell.translate(None, string.digits))
-    
-    num_skipcols = [helpers.col_to_number(col) for col in skipcols]    
-    
-    rows_to_write = [row for row in range(start_row, start_row + num_rows) if \
-        row not in skiprows]
-    cols_to_write = [col for col in range(start_col, start_col + num_cols) if \
-        col not in num_skipcols]
-    
+    start_col = helpers.col_to_number(xls_startcell.translate(None,
+                                                              string.digits))
+
+    num_skipcols = [helpers.col_to_number(col) for col in skipcols]
+
+    rows_to_write = [row for row in range(start_row, start_row + num_rows) if
+                     row not in skiprows]
+    cols_to_write = [col for col in range(start_col, start_col + num_cols) if
+                     col not in num_skipcols]
+
     for row_idx, row in enumerate(rows_to_write):
         for col_idx, col in enumerate(cols_to_write):
             current_cell = helpers.cell_name(row, col)
-            
+
             # This will not work if the numpy data type cannot be converted
             # easily into a native python one.
             sheet[current_cell] = table_data.iloc[row_idx, col_idx].item()
-            
