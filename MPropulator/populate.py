b7d27ee3af68f212dd84fc7fca06d7a019d165ff
Franco Solleza <solleza.f@gmail.com>
2015-11-04 10:52:31 -0500

    moving write tab out

--- MPropulator/populate.py
+++ MPropulator/populate.py
@@ -76,34 +76,4 @@ def populate(config, shell_xls, output_xls=None):
     workbook.save(output_xls)
 
 
-def write_tab(sheet, table_data, xls_startcell, skiprows, skipcols):
-    """Writes the data for a particular table to the corresponding
-    Excel spreadsheet.
 
-    sheet: openpyxl worksheet to which you're writing
-    table_data: pandas data frame containing data to write
-    xls_startcell: cell in the sheet at which you will begin writing
-    skiprows: list of rows in Excel spreadsheet to skip
-    skipcols: list of columns in Excel spreadsheet to skip
-    """
-    num_rows = table_data.shape[0]
-    num_cols = table_data.shape[1]
-
-    start_row = int(xls_startcell.translate(None, string.ascii_letters))
-    start_col = helpers.col_to_number(xls_startcell.translate(None,
-                                                              string.digits))
-
-    num_skipcols = [helpers.col_to_number(col) for col in skipcols]
-
-    rows_to_write = [row for row in range(start_row, start_row + num_rows) if
-                     row not in skiprows]
-    cols_to_write = [col for col in range(start_col, start_col + num_cols) if
-                     col not in num_skipcols]
-
-    for row_idx, row in enumerate(rows_to_write):
-        for col_idx, col in enumerate(cols_to_write):
-            current_cell = helpers.cell_name(row, col)
-
-            # This will not work if the numpy data type cannot be converted
-            # easily into a native python one.
-            sheet[current_cell] = table_data.iloc[row_idx, col_idx].item()
