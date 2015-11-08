e707a16142621d28467c0d8f159497ff09d188d8
raymondnoonan <ray.noonan32@gmail.com>
2015-11-08 16:12:49 -0500

    Clean up populate file, move read_config to its own thing

--- MPropulator/populate.py
+++ MPropulator/populate.py
@@ -1,11 +1,13 @@
 import helpers
 import pandas as pd
 import openpyxl
-import ast
-import string
 import os
+import string
 import re
 import warnings
+from writetab import write_tab
+from read_config import read_config
+
 
 def populate(config, shell_xls, output_xls=None):
     """Populates an Excel spreadsheet from a shell.
@@ -15,50 +17,44 @@ def populate(config, shell_xls, output_xls=None):
     output_xls: string of XLSX file address for output file
                 if output_xls is not specified, overwrite shell_xls
     """
-
     # TODO: move to validate populate input function
     if not os.path.isfile(config):
-        raise ValueError("config file not found");
+        raise ValueError("config file not found")
     if not os.path.isfile(shell_xls):
-        raise ValueError("shell not found");
+        raise ValueError("shell not found")
 
-    if output_xls == None:
-        warnings.warn("output_xls is not specified - this function is overwriting shell_xls")
+    if output_xls is None:
+        warnings.warn("output_xls is not specified -"
+                      "this function is overwriting shell_xls")
         output_xls = shell_xls
 
     pathReg = re.compile("(.+)(\/.+\..+$)")
     path = pathReg.findall(output_xls)
     path = path[0][0]
     if not os.path.isdir(path):
-        raise ValueError("Output paths not found");
+        raise ValueError("Output paths not found")
 
     workbook = openpyxl.load_workbook(shell_xls)
 
-    # Move to read config function
-    convert_cols = {
-        'csv_startcell': ast.literal_eval,
-        'skiprows': ast.literal_eval,
-        'skipcols': ast.literal_eval
-    }
+    parsed_config = read_config(config)
 
-    parsed_config = pd.read_csv(config, converters=convert_cols)
     # Todo Validate Config File Function
     parsed_config['ignore'].fillna(False, inplace=True)
-    tabSet = set(parse_config['tabname'])
+    tabSet = set(parsed_config['tabname'])
 
     wbSheetList = workbook.get_sheet_names()
-    wbSheetSet = set(wbSheetNames)
+    wbSheetSet = set(wbSheetList)
     assert len(wbSheetList) == len(wbSheetSet)
-    if not tabset.issubset(wbSheetSet):
-        raise ValueError("There are Tabs in your config that are not in the shell")
+    if not tabSet.issubset(wbSheetSet):
+        raise ValueError("There are Tabs in your config"
+                         "that are not in the shell")
 
-    
     for enum, table in parsed_config.iterrows():
         if table['ignore'] is not True:
             sheet = workbook.get_sheet_by_name(table['tabname'])
 
             csv_startcell = table['csv_startcell']
-            csv_start_row = int(csv_startcell.translate(None,string.letters))
+            csv_start_row = int(csv_startcell.translate(None, string.letters))
             csv_start_col = csv_startcell.translate(None, string.digits)
 
             table_data = pd.read_csv(table['csv'], skiprows=csv_start_row-1)
