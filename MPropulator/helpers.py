104e05f326b7138e524296c049a1860c8c8a8cea
raymondnoonan <ray.noonan32@gmail.com>
2015-11-09 20:50:00 -0500

    document col_to_number

--- MPropulator/helpers.py
+++ MPropulator/helpers.py
@@ -1,5 +1,6 @@
 import string
 
+
 def column_range(start, stop, skip_columns=None):
     """0-indexed function that returns a list of Excel column names, except
     for skip_columns
@@ -17,7 +18,7 @@ def column_range(start, stop, skip_columns=None):
     if stop < 0:
         raise ValueError("Stop must be >= 0")
 
-    return [column_name(i + 1) for i in range(start, stop) \
+    return [column_name(i + 1) for i in range(start, stop)
             if column_name(i + 1) not in skip_columns]
 
 
@@ -40,15 +41,26 @@ def column_name(col):
         excel_col = chr(mod + ord('A')) + excel_col
 
     return excel_col
-    
+
+
 def col_to_number(col):
+    """0-indexed function that, given a column name, returns
+    the number of that column in excel.
+
+    col: string consisting of Excel column name
+    return: int that corresponds to excel column name
+
+    col_to_number("A") => 0
+    col_to_number("AB") => 28
+    """
     num = 0
     for c in col:
         if c in string.ascii_letters:
-            num = num * 26 +  (ord(c.upper()) - ord('A')) + 1
+            num = num * 26 + (ord(c.upper()) - ord('A')) + 1
         else:
             raise ValueError("Input had characters other than ASCII letters")
     return num
+
 
 def cell_name(row, col):
     """ 0-indexed function that, given a row and column number,
